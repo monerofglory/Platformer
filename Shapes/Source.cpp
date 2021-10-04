707f6c1a2aebecd5c0eac85e7bc35a1f5e9319ea
Alex <Alex@ALEX-PC>
2021-10-04 16:36:37 +0100

    Added additional code optimisations

--- Shapes/Source.cpp
+++ Shapes/Source.cpp
@@ -5,6 +5,7 @@
 #include <list>
 #include <vector>
 #include <string>
+#include <ctime>
 
 #include "Player.h"
 
@@ -34,17 +35,26 @@ vector<int> gameFloor = { 0, 0, windowX, 8 };
 //List of all platforms
 vector<vector<int>> platforms;
 
+//star score
+vector<int> score;// = { 50, 50, 50, 50 };
+
 void createPlatforms() {
-	srand(GLUT_ELAPSED_TIME);
-	int numberOfPlatforms = rand() % 5 + 1;
+	srand(time(0));
+	int numberOfPlatforms = 3;//rand() % 5 + 2;
 	for (int i = 0; i <= numberOfPlatforms; i++) {
 		vector<int> p = { 
-			rand() % windowX,  //X position
+			rand() % (windowX - 250) + 50 ,  //X position
 			(100 * (i + 1)),  //Y position
-			rand() % 150 + 75,  //Width
+			rand() % 150 + 125,  //Width
 			25 }; //Height
 		platforms.push_back(p); //Add to list of platforms.
 	}
+}
+
+void generateScore() {
+	srand(time(0));
+	int platformChoice = rand() % platforms.size();
+	score = { platforms.at(platformChoice).at(0) + (platforms.at(platformChoice).at(2) / 2), platforms.at(platformChoice).at(1) + platforms.at(platformChoice).at(3) + 10, 25, 25 };
 }
 
 void myinit(void)
@@ -54,6 +64,7 @@ void myinit(void)
 	glLoadIdentity();
 	gluOrtho2D(0.0, windowX, 0.0, windowY);
 	createPlatforms();
+	generateScore();
 }
 
 void setPixel(GLint x, GLint y)
@@ -65,9 +76,13 @@ void setPixel(GLint x, GLint y)
 
 //Function for drawing the platform.
 //Takes in the current co-ordinates, and a width and height of the rectangle.
-void drawPlatform(vector<int> coords) {
-	testCounter++;
-	glColor3ub(255, 0.0, 0.0); //Set colour
+void drawPlatform(vector<int> coords, string type) {
+	if (type == "platform") {
+		glColor3ub(255, 0.0, 0.0); //Set colour
+	}
+	else {
+		glColor3ub(255, 255, 0.0);
+	}
 	glBegin(GL_QUADS);
 	//Set vertices
 	glVertex2i(coords.at(0), coords.at(1));
@@ -82,7 +97,7 @@ void drawPlatform(vector<int> coords) {
 //Function for drawing ALL platforms.
 void drawAllPlatforms(vector<vector<int>> platforms) {
 	for (int i = 0; i <= platforms.size() - 1; i++) {
-		drawPlatform(platforms.at(i));
+		drawPlatform(platforms.at(i), "platform");
 	}
 }
 
@@ -108,8 +123,11 @@ void display()
 	glPointSize(2.0);
 	//Draw platform and player.
 	drawPlayer(getPlayerX(), getPlayerY());
-	drawPlatform(gameFloor);
+	drawPlatform(gameFloor, "platform");
+	glutSwapBuffers();
 	drawAllPlatforms(platforms);
+	glutSwapBuffers();
+	drawPlatform(score, "score");
 	//Flush to screen.
 	glFlush();
 }
@@ -162,6 +180,7 @@ bool checkIfPlayerInsideHorizontal(vector<int> p, vector<int> obj) {
 	return inside;
 }
 
+int previousTopCheckCounter = 0;
 void checkCollisions() {
 	//Get player positions
 	int px = getPlayerX();
@@ -180,27 +199,33 @@ void checkCollisions() {
 			setColliding("top", true);
 			setPlayerY(platforms.at(i).at(1) - playerHeight);
 			previousTop = true;
-			printf("TOP\n");
 		}
 		else if (checkIfPlayerInsideHorizontal(playerBottom, platforms.at(i))) {
 			if (!previousTop) {
 				setPlayerY(platforms.at(i).at(1) + platforms.at(i).at(3));
 				setColliding("bottom", true);
-				printf("BOTTOM\n");
 			}
 		}
 		else if ((checkIfPlayerInsideVertical(playerLeft, platforms.at(i))) || (checkIfPlayerInsideVertical(playerRight, platforms.at(i))) ) {
-			//Check if still on a platform
-			//if (!checkIfPlayerInsideVertical(playerBottom, platforms.at(i))) {
-			//	printf("SIDE\n");
-			//	setColliding("side", true);
-			//}
 			if (isPlayerJumping()) {
 				setColliding("side", true);
 			}
 			previousTop = false;
 		}
-	
+		else {
+			if (previousTopCheckCounter >= 10) {
+				previousTop = false;
+				previousTopCheckCounter = 0;
+			}
+			else {
+				previousTopCheckCounter++;
+			}
+		}
+		//Check if collected score
+		if ((checkIfPlayerInsideVertical(playerTop, score)) || (checkIfPlayerInsideVertical(playerBottom, score)) || (checkIfPlayerInsideVertical(playerLeft, score)) || (checkIfPlayerInsideVertical(playerRight, score))) {
+			//Collected!
+			generateScore();
+		}
 	}
 	//Check collision with floor
 	if (checkIfPlayerInsideVertical(playerBottom, gameFloor)) {
@@ -243,7 +268,8 @@ int main(int argc, char** argv)
 	glutInit(&argc, argv);
 	glutInitWindowSize(windowX, windowY);
 	glutInitWindowPosition(10, 10);
-	glutCreateWindow("User_Name");
+	glutCreateWindow("Platformer");
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	myinit();
 	glutDisplayFunc(display);
 	glutIdleFunc(update);
